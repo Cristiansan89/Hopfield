9c1a10ae298ccb624c605e0f7c28d1f5212520f6
enrique gauto sand <enriquegautosand@hotmail.com>
2022-02-10 13:19:30 -0300

    v--4

--- server.py
+++ server.py
@@ -1,11 +1,42 @@
 from distutils.log import debug
 from flask import Flask, render_template
+#nuevos
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+import json as js
+import os
+import pickle
+dbdir = "sqlite:///" + os.path.abspath(os.getcwd()) + "/database.db"
+
 app = Flask(__name__)
+#nuevos
+app.config["SQLALCHEMY_DATABASE_URI"] =dbdir
+db = SQLAlchemy(app)
+
+from models import *
+
 @app.route("/")
 def index():
     return render_template("code.html")
+@app.route("/cargar")
+def cargar():
+    lst = [1, 2, 3]
 
+    serializado = js.dumps(lst)
+    print(serializado, type(serializado))
+    nueva_imagen=Imagen(imagen=serializado)
+    db.session.add(nueva_imagen)
+    db.session.commit()
+    return render_template("code.html")
+@app.route("/mostrar")
+def mostrar():
+    primera_imagen=Imagen.query.first()
+    #filtrado primera_imagen=Imagen.query.filter_by(id=1).first()
+    deserialized = js.loads(primera_imagen.imagen)
+    print(deserialized, type(deserialized))
+    return render_template("code.html")
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
 
 
