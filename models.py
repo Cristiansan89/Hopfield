a2973fd7458c3cbf68956ce0dfcda086825839a7
Cristian <crisanz89@gmail.com>
2022-02-16 21:15:53 -0300

    v-15

--- models.py
+++ models.py
@@ -3,11 +3,15 @@ from sqlalchemy.ext.mutable import MutableList
 from server import db
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
+import json as js
+
 class Imagen(db.Model):
     __tablename__ = 'imagen'
     id = Column(Integer, primary_key=True)
     patron = Column(String)
     letra = Column(String)
+    def convertijson(self): 
+        return js.loads(self.patron) 
 
 class Aprendizaje(db.Model):    
     __tablename__ = 'aprendizaje'
